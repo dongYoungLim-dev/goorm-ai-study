c2d86c9cdbb030f628c750b723983960e0a205eb
dyMacBookPro <ldy6740@naver.com>
2026-01-14 22:58:04 +0900

    설명 추가

--- 260114/REST_API_Email/app.py
+++ 260114/REST_API_Email/app.py
@@ -1,8 +1,10 @@
-import requests
-import os
-import smtplib
-from email.mime.text import MIMEText
-from dotenv import load_dotenv
+import requests # RESTful API 사용 Library
+import smtplib # 메일 발송 Library
+from email.mime.text import MIMEText # 메일의 body의 내용을 작성하기 위한 Library
+# 이메일은 Multipurpose Internet Mail Extensions 이라는 표준 형식에 맞게 작성을 해야 하는데 이를 도와준다.
+
+from dotenv import load_dotenv # env 파일을 읽어 오는 라이브러리
+import os # 파일 생성, 폴더 탐색, 환경 변수 읽기 위에 os와 소통하는 Library
 
 # .env 값을 가지고 오기 위해 load_dotenv 최초 1회 실행
 load_dotenv()
@@ -13,7 +15,7 @@ print(response.json())
 
 sendEmail = "ldy6740@naver.com"
 recvEmail = "ldy6740@gmail.com"
-password = os.environ.get('EMAIL_PW')
+password = os.environ.get('EMAIL_PW') # .env 파일에 있는 'EMAIL_PW' 내용으로 치환된다. 
 
 smtpName = "smtp.naver.com"
 smtpPort = "587"
